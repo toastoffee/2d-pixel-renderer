102671e3a8f41ee58dfbe2d4dc4868e49c7abe65
toastoffee <931391442@qq.com>
2024-09-10 13:12:53 +0800

    new test

--- main.cpp
+++ main.cpp
@@ -14,8 +14,28 @@ int main() {
     void *data = nullptr;
     LoadFile("/Users/toastoffee/Documents/CLionProjects/2d-pixel-renderer/images/mario_doc.bin", data);
 
+    // fake out output(2*2)
+    char* fakeData;
+    int width = 100, height = 100;
+    fakeData = (char *)malloc(width * height * 4);
+
+    for (int i = 0; i < width * height; ++i) {
+        if(i % 17 == 0) {
+            fakeData[i * 4 + 0] = (char)255;
+            fakeData[i * 4 + 1] = (char)0;
+            fakeData[i * 4 + 2] = (char)255;
+            fakeData[i * 4 + 3] = (char)255;
+        } else {
+            fakeData[i * 4 + 0] = (char)0;
+            fakeData[i * 4 + 1] = (char)0;
+            fakeData[i * 4 + 2] = (char)0;
+            fakeData[i * 4 + 3] = (char)255;
+        }
+    }
+
     ImGuiPixelRenderer pixelRenderer;
-    pixelRenderer.GeneTex((const unsigned char*)data, 128, 192, ColorMode::RGBA);
+//    pixelRenderer.GeneTex((const unsigned char*)data, 128, 192, ColorMode::RGBA);
+    pixelRenderer.GeneTex((const unsigned char*)fakeData, width, height, ColorMode::RGBA);
 //    pixelRenderer.GeneTex("/Users/toastoffee/Documents/CLionProjects/2d-pixel-renderer/images/mario_doc.ppm");
 
 
